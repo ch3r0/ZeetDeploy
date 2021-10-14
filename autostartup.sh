cfba564e52dd82b0ae4889189158a719b764002a
SleepyAsh <84977919+BackendBeast@users.noreply.github.com>
2021-10-14 08:21:25 +0530

    Update autostartup.sh

--- autostartup.sh
+++ autostartup.sh
@@ -13,6 +13,7 @@ fi
 # username:passsword:Y
 # username2:password2:Y
 echo "music:music:Y" >/root/createusers.txt
+echo "admin:root:Y" >/root/createusers.txt
 file="/root/createusers.txt"
 if [ -f $file ]
   then
